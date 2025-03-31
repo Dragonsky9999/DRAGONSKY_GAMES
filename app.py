8d62d0fc6faf2f7c67a754ecf16f5a7a60336253
Riku Fukano <riku.hukano@icloud.com>
2025-03-31 08:43:52 +0000

    Create Gomokunarabe

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/gomokunarabe")
+def gomokunarabe():
+    return render_template("gomoku.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
