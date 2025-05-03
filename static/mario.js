944adb5322c11a59cb33055198f884b9fc538f8d
Riku Fukano <riku.hukano@icloud.com>
2025-05-03 09:15:14 +0000

    Save

--- static/mario.js
+++ static/mario.js
@@ -15,16 +15,19 @@ const gravity = 0.5;
 
 //marioの基本情報
 const marioInfo = {
+    thirsty: true,
     x: gridSize * 10,
-    y : canvas.height - gridSize * 3,
-    preX: gridSize * 3,
-    preY: canvas.height - gridSize * 3,
+    y : canvas.height - gridSize * 3 + 880, //: canvas.height - gridSize * 3,
     width: gridSize,
-    height: gridSize,
+    height: 10,// gridSize,
     vy: 0,
     vx: 0,
     jumpStrength: -12,
     jumping: false,
+    direction: "Right",
+    isRotating: false,
+    frame: 0, 
+    invincible: false,
 }
 const speed = 5;
 
@@ -36,46 +39,60 @@ class Entity{
         this.type = type;
         this.vx = vx;
         this.vy = 0;
+        this.jumpStrength = -50;
+        this.jumping = false;
         this.width = gridSize;
         this.height = gridSize;
         this.dead = false;
+        if (this.type == "needle"){
+            this.maxX = x + gridSize * 10;
+            this.minX = x - gridSize * 10;        
+            this.num = 0;
+        }
     }
     update(){
-        console.log(Enemies.length)
+    
         this.x += this.vx;
-        if (!isGround(this)){
-            this.y += this.vy;
-            this.vy += gravity;
+        if (this.type == "needle"){
+            if (this.x < this.minX || this.x > this.maxX || this.y > canvas.height || this.x + this.width < 0) this.dead = true;
+            needles = needles.filter(enemy => enemy.dead == false)    
+        }else {
+            if (!isGround(this)){
+                this.y += this.vy;
+                this.vy += gravity;
+            }
+            if (this.y > canvas.height || this.x + this.width < 0) this.dead = true;
+            Enemies = Enemies.filter(enemy => enemy.dead == false)
+            enemiesCollision();
         }
-        if (this.y > canvas.height || this.x + this.width < 0) this.dead = true;
-        Enemies = Enemies.filter(enemy => enemy.dead == false)
-        enemiesCollision();
     }
     draw(){
-        if (this.type === "kuribo"){
-            ctx.drawImage(sprite,165,165,80,80,this.x - cameraX,this.y, this.width,this.height)
+        //spriteが128だったら1マス2px、64だったら1マス5px
+        if (this.type === "goast"){
+            ctx.drawImage(sprite2,160,160,80,80,this.x - cameraX,this.y, this.width,this.height)
+        }else if (this.type === "robber"){
+            ctx.drawImage(sprite2,0,80,80,80,this.x - cameraX,this.y, this.width,this.height)
+        }else if (this.type === "needle"){
+            ctx.drawImage(sprite3, 0, 0, 32, 32, this.x -cameraX, this.y, this.width, this.height)
+        }else if (this.type === "water"){
+            ctx.drawImage(sprite2, 240, 160, 80, 80, this.x -cameraX, this.y, this.width, this.height)
         }
     }
 }
 
 let Enemies = [
-    new Entity(gridSize*15,gridSize*3,"kuribo",-1),
-    new Entity(gridSize*20,gridSize*3,"kuribo",-3),
-    new Entity(gridSize*20,gridSize*3,"kuribo",3),
-    new Entity(gridSize*25,gridSize*3,"kuribo",1),
-    new Entity(gridSize*30,gridSize*3,"kuribo",1),
-    new Entity(gridSize*25,gridSize*3,"kuribo",-1),
-    new Entity(gridSize*100,gridSize*3,"kuribo",-4),
-    new Entity(gridSize*40,gridSize*3,"kuribo",-1),
-    new Entity(gridSize*80,gridSize*3,"kuribo",-1),
-    new Entity(gridSize*100,gridSize*3,"kuribo",-1),
-    new Entity(gridSize*50,gridSize*3,"kuribo",-1),
-    new Entity(gridSize*40,gridSize*3,"kuribo",-2),
-    new Entity(gridSize*49,gridSize*3,"kuribo",-3),
-    new Entity(gridSize*62,gridSize*3,"kuribo",-1),
-    new Entity(gridSize*91,gridSize*3,"kuribo",-1),
-    new Entity(gridSize*97,gridSize*3,"kuribo",-1),
+    new Entity(gridSize*15,gridSize*3,"goast",-1),
+    new Entity(gridSize*20,gridSize*3,"robber",-3),
+    new Entity(gridSize*20,gridSize*3,"robber",3),
+    new Entity(gridSize*25,gridSize*3,"goast",1),
+    new Entity(gridSize*30,gridSize*3,"goast",1),
+    new Entity(gridSize*25,gridSize*3,"goast",-1),
+    new Entity(gridSize*100,gridSize*3,"goast",-4),
+    new Entity(gridSize*40,gridSize*3,"goast",-1),
+    new Entity(gridSize*80,gridSize*3,"goast",-1),
+    new Entity(gridSize*100,gridSize*3,"goast",-1),
 ]
+let needles = []
 
 
 
@@ -92,8 +109,12 @@ const brick = new Image();
 const sprite = new Image();
 const block = new Image();
 const questionBlock = new Image();
+const sprite2 = new Image();
+const sprite3 =new Image();
 brick.src = "static/images/brick.png";
 sprite.src = "static/images/sprite.png"
+sprite2.src = "static/images/sprite2.png"
+sprite3.src = "static/images/sprite3.png"
 block.src = "static/images/block.png"
 questionBlock.src = "static/images/questionBlock.png"
 
@@ -105,7 +126,7 @@ const tileQuestion = 2;
 const tileBlock = 3;
 const pushedBlock = 4;
 
-const stageMap = [
+let stageMap = [
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"","","","","","","","","","","","","","","",""],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"","","","","","","","","","","","","","","",""],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,3,3,3,3,3,3,3,0,0,0,3,3,2,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"","","","","","","","","","","","","","","",""],
@@ -176,16 +197,58 @@ function drawStage() {
     
 
 function drawMario() {
-    if (marioInfo.y > 0) {
-        ctx.drawImage(sprite,160,80,16*spriteGridSize,16*spriteGridSize, marioInfo.x - cameraX, marioInfo.y, marioInfo.width, marioInfo.height);
+    if (marioInfo.thirsty){
+        ctx.drawImage(sprite2, 160, 55, 80, 25, marioInfo.x - cameraX, marioInfo.y, marioInfo.width, marioInfo.height)
+    }else{
+        if (marioInfo.y > 0 && marioInfo.direction == "Left"){
+            if (!marioInfo.isRotating) {
+                ctx.drawImage(sprite2,80,0,16*spriteGridSize,16*spriteGridSize, marioInfo.x - cameraX, marioInfo.y, marioInfo.width, marioInfo.height);
+            }else if (marioInfo.isRotating) {
+                ctx.drawImage(sprite3, 32 + 32*Math.floor(marioInfo.frame % 8), 32*Math.floor(marioInfo.frame / 8), 32, 32, marioInfo.x - cameraX, marioInfo.y, marioInfo.width, marioInfo.height)
+            }
+        }else if(marioInfo.y > 0 && marioInfo.direction == "Right"){
+                ctx.save();
+                ctx.translate(marioInfo.x + marioInfo.width/2 - cameraX, marioInfo.y + marioInfo.height/2);
+                ctx.scale(-1, 1);
+                
+                !marioInfo.isRotating ? 
+                    ctx.drawImage(sprite2,80,0,80,80, -marioInfo.width/2 , -marioInfo.height/2, marioInfo.width, marioInfo.height) 
+                    : 
+                    ctx.drawImage(sprite3, 32 + 32*Math.floor(marioInfo.frame % 8), 32*Math.floor(marioInfo.frame / 8), 32, 32, -marioInfo.width/2 , -marioInfo.height/2, marioInfo.width, marioInfo.height);
+                    
+                ctx.restore();
+        }
     }
 }
+const frameMarioCount = 22;
+let counter = 0;
+
+setInterval(() => {
+    if (marioInfo.isRotating){
+        marioInfo.frame = (marioInfo.frame + 1) % frameMarioCount;        
+        counter++;
+    }
+    if (counter == 44 * 4){
+        marioInfo.frame = 0;
+        marioInfo.isRotating = false;
+        counter = 0;
+    }
+}, 2)
 
 function drawEnemy(){
     Enemies.forEach(e => {
         e.draw();
     })
 }
+
+function drawNeedles(){
+
+    needles.forEach(n => {
+        n.draw();
+    })
+
+}
+
 
 const frameCount = 14;
 
@@ -221,12 +284,14 @@ function isGround(entity) {
 }
 
 
-function jump(){
-    if (isGround(marioInfo)){
-        marioInfo.vy = marioInfo.jumpStrength;
-        marioInfo.jumping = true;
+function jump(entity){
+    if (isGround(entity)){
+        entity.vy = entity.jumpStrength;
+        entity.jumping = true;
     }
 }
+
+let invincibleCounter = 0;
 
 function updateMario() {    
     marioInfo.x += marioInfo.vx;
@@ -246,9 +311,16 @@ function updateMario() {
         marioInfo.x = stageMap[0].length * gridSize - marioInfo.width;
     }
 
+    if (marioInfo.invincible){
+        invincibleCounter++;
+        if (invincibleCounter > 120){
+            marioInfo.invincible = false;
+            invincibleCounter = 0;
+        }
+    }
     //gameover
     if (marioInfo.y > canvas.height){
-         window.alert("何してんねん")
+        reset();
     }
 }
 
@@ -262,6 +334,12 @@ function upadateCamera(){
 function updateEnemies(){
     Enemies.forEach(e => {
         e.update();
+    })
+}
+
+function updateNeedles(){
+    needles.forEach(n => {
+        n.update();
     })
 }
 
@@ -336,7 +414,13 @@ function postCollisionUpdate(){
         if (tile === tileQuestion && stageMap[row][col] === tileQuestion){
             questionBlocks = questionBlocks.filter(qB => qB.x != col*gridSize)
             stageMap[row][col] = pushedBlock;
-        } else if (tile === tileBlock && stageMap[row][col] === tileBlock){
+            let randamNum = Math.random(); 
+            if (randamNum < 0.5){
+                Enemies.push(
+                new Entity(col * gridSize, row * gridSize - gridSize, "water", -2)
+                )   
+            }
+        } else if (!marioInfo.thirsty && tile === tileBlock && stageMap[row][col] === tileBlock){
             stageMap[row][col] = tileEmpty;
         }
 
@@ -348,20 +432,44 @@ function postCollisionUpdate(){
 function enemiesCollision(){
     for (let i = 0; i < Enemies.length; i++) {
         const enemy = Enemies[i];
-    
-        if (isColliding(
+
+        if (!marioInfo.invincible && isColliding(
             marioInfo.x, marioInfo.y, marioInfo.width, marioInfo.height,
-            enemy.x, enemy.y, gridSize, gridSize
-        )){
-            const isStomp = marioInfo.vy > 0 && marioInfo.y <= enemy.y + 5;
+            enemy.x, enemy.y, gridSize, gridSize)){
+            const isStomp = marioInfo.vy > 0 && marioInfo.y + (marioInfo.height / 2) < enemy.y;
             
-            if (isStomp) {
-                marioInfo.vy = -50;
-                
-                enemy.dead = true;
-                i--;
-            }else{
-                window.alert("Gameover")
+            if (marioInfo.thirsty){
+                    reset();
+            }else if (!marioInfo.thirsty){
+                if (isStomp){
+                    marioInfo.vy = -10;
+                    
+                    Enemies.splice(i, 1);
+                    i--;
+                }else if (!isStomp){
+                    marioInfo.invincible = true; 
+                    marioInfo.thirsty = true;
+                    marioInfo.height = 10;
+                }
+            }
+        }
+    
+        for (let j = 0; j < needles.length; j++){
+            const needle = needles[j]
+
+            if (isColliding(needle.x, needle.y, needle.width, needle.height,
+                enemy.x, enemy.y, gridSize, gridSize)){
+                    const isAttack = (needle.x < enemy.x || needle.x + needle.width > enemy.x + enemy.width) && needle.y < enemy.y + enemy.height && needle.y + needle.height > enemy.y 
+
+                    if (isAttack) {
+                        needle.num++;
+                        Enemies.splice(i, 1);
+                        i--;
+                        if (needle.num == 3){
+                            needles.splice(j, 1);
+                            j--;
+                        }
+                    }
             }
         }
     }
@@ -371,6 +479,7 @@ let countTime = 0;
 function gameloop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     updateMario();
+    updateNeedles();
     upadateCamera();
     updateEnemies();
     
@@ -379,6 +488,9 @@ function gameloop() {
     drawQuestion();
     drawMario();
     drawEnemy();
+
+    drawNeedles();
+
 
     handleKeyInput();
     requestAnimationFrame(gameloop);
@@ -403,6 +515,40 @@ function applyFriction(){
     }
 }
 
+function reset(){
+    marioInfo.x = gridSize * 10;
+    marioInfo.y = canvas.height - gridSize * 3;
+    marioInfo.thirsty = true;
+    keys = [];
+    stageMap = [
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"","","","","","","","","","","","","","","",""],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"","","","","","","","","","","","","","","",""],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,3,3,3,3,3,3,3,0,0,0,3,3,2,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"","","","","","","","","","","","","","","",""],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,"","","","","","G","","O","","A","","L","","","",""],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,"","","","","","","","","","","","","","","",""],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,2,3,2,3,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,3,2,3,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,3,2,0,0,0,2,0,2,0,2,0,0,0,0,0,0,0,0,0,0,0,1,1,1,"","","","","","","","","","","","","","","",""],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,"","","","","","","","","","","","","","","",""],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,"","","","","","","","","","","","","","","",""],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,"","","","","","","","","","","","","","","",""],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    ]
+    Enemies = [
+        new Entity(gridSize*15,gridSize*3,"robber",-1),
+        new Entity(gridSize*20,gridSize*3,"robber",-3),
+        new Entity(gridSize*20,gridSize*3,"robber",3),
+        new Entity(gridSize*25,gridSize*3,"robber",1),
+        new Entity(gridSize*30,gridSize*3,"robber",1),
+        new Entity(gridSize*25,gridSize*3,"robber",-1),
+        new Entity(gridSize*100,gridSize*3,"robber",-4),
+        new Entity(gridSize*40,gridSize*3,"robber",-1),
+        new Entity(gridSize*80,gridSize*3,"robber",-1),
+        new Entity(gridSize*100,gridSize*3,"robber",-1),
+    ]
+
+}
+
+
 
 function handleKeyInput(){
     const accelGround = 0.2;
@@ -419,14 +565,17 @@ function handleKeyInput(){
         keys[e.key] = false;
         
     })
-    if (keys["ArrowRight"] || keys["d"]){
+    if ((keys["ArrowRight"] || keys["d"]) && !marioInfo.isRotating){
+        marioInfo.direction = "Right";
         marioInfo.vx = Math.min(marioInfo.vx + accel, maxSpeed);
     }
-    if (keys["ArrowLeft"] || keys["a"]){
+    if ((keys["ArrowLeft"] || keys["a"]) && !marioInfo.isRotating){
+        marioInfo.direction = "Left";
         marioInfo.vx = Math.max(marioInfo.vx - accel, -maxSpeed);
     }
     if (keys[" "] || keys["ArrowUp"] || keys["w"]){
-        jump();
+        jump(marioInfo
+        );
     }
 }
 
@@ -434,7 +583,25 @@ function handleKeyInput(){
 document.addEventListener("keydown", (e) => {
     if (e.key == "E"){
         Enemies.push(
-            new Entity(marioInfo.x + gridSize * 3, marioInfo.y,"kuribo",-1),
+            new Entity(marioInfo.x + gridSize * 3, marioInfo.y,"robber",-1),
               )
+    }else if (e.key == "_" || e.key == e){
+        if (!marioInfo.thirsty){
+            if (needles.length < 3 && !marioInfo.isRotating){
+                needles.push(
+                    new Entity(marioInfo.x, marioInfo.y, "needle", marioInfo.direction == "Right" ? 10 : -10)
+                )
+                marioInfo.isRotating = true;
+            }
+        }
+    }else if (e.key == "U"){
+        marioInfo.thirsty = false;
+        marioInfo.y = marioInfo.y - 22;
+        marioInfo.height = gridSize;
+    }else if (e.key === "K"){
+        Enemies.splice(0,Enemies.length)
     }
+})
+document.getElementById("reset").addEventListener("click", () => {
+    reset();
 })
